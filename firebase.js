ae6f0b8daef1acc2e2fe70ab6dc78baa0f05ca9d
André Marques <111473714+TheBigLaw@users.noreply.github.com>
2026-01-30 15:50:31 -0300

    Update Firebase configuration and login handling
    Refactor Firebase configuration and simplify anonymous login.

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// CONFIGURAÇÃO
+// CONFIGURAÇÃO FIREBASE
 firebase.initializeApp({
   apiKey: "AIzaSyA-VlrQcVxkoiAjOAIEFXrX_RYjN2Bf-WE",
   authDomain: "financasequilibrium-7c2b4.firebaseapp.com",
@@ -8,14 +8,7 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-let userId = null;
-
-// LOGIN ANÔNIMO AUTOMÁTICO
-auth.signInAnonymously()
-  .then(cred => {
-    userId = cred.user.uid;
-    carregarDados();
-  })
-  .catch(err => {
-    console.error("Erro no login Firebase", err);
-  });
+// login anônimo
+auth.signInAnonymously().catch(err => {
+  console.error("Erro no login Firebase", err);
+});
